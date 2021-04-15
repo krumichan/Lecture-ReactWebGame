0d99322fd71c71c396685bc4cce48e2dff300ab5
KrumiChan <lovelybarry@naver.com>
2021-04-15 14:41:30 +0900

    [webpack] word relay hot loading 추가.

--- lecture/webpackWordRelay/webpack.config.js
+++ lecture/webpackWordRelay/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');  // node의 path library.
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 // 파일을 하나로 합쳐주기 위한 webpack 설정.
 module.exports = {
@@ -40,19 +41,44 @@ module.exports = {
                         }]
                         , '@babel/preset-react'
                     ]
-                    , plugins: ['@babel/plugin-proposal-class-properties']
+                    , plugins: [
+                        '@babel/plugin-proposal-class-properties'
+                        , 'react-refresh/babel'
+                    ]
                 }
             }
         ]
     }
 
     , plugins: [
-
+        // # npm run dev
+        // 위의 명령어로 실행해야 한다.
+        // # npx webpack
+        // 위의 명령어는 배포 명령어이기 때문에,
+        // TypeError [ERR_INVALID_ARG_TYPE]: The "data" argument must be of type string or an instance of Buffer, TypedArray, or DataView. Received undefined
+        // 위와 같은 에러가 발생한다.
+        // 배포시에는 아래의 코드를 주석 처리 해야한다.
+        new RefreshWebpackPlugin()
+        ,
     ]
 
     // 출력
     , output: {
+        // path는 absolute path이다.
         path: path.join(__dirname, 'core') // __dirname : current directory. ( 여기서는 webpackWordRelay directory absolute path )
         , filename: 'app.js'
+
+        // publicPath는 relative path이다. ( 가상 경로 )
+        , publicPath: '/core/'
+    }
+
+    /**
+     * build 결과물을 아래 publicPath를 메모리로 저장해 두는데,
+     * devServer는 소스 등의 변경을 감지하여,
+     * 그에 따라 저장한 결과물을 자동으로 수정해준다.
+     */
+    , devServer: {
+        publicPath: '/core/'
+        , hot: true
     }
 }
