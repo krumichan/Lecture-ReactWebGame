23c685ac2f851e2c061e11f005a199d6729bddac
KrumiChan <lovelybarry@naver.com>
2021-04-15 13:34:53 +0900

    webpack.config.js 예시 설정 내용 추가.

--- lecture/webpackTimesTable/webpack.config.js
+++ lecture/webpackTimesTable/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
     mode: 'development'
@@ -14,17 +15,32 @@ module.exports = {
     }
 
     , module: {
-        rules: [
-            {
-                test: /\.jsx?$/
-                , loader: 'babel-loader'
-                , options: {
-                    presets: ['@babel/preset-env', '@babel/preset-react']
-                    , plugins: []
-                }
+        rules: [{
+            test: /\.jsx?$/
+            , loader: 'babel-loader'
+            , options: {
+                // plugin의 모임이 preset.
+                presets: [
+                    // 참고 URL : https://github.com/browserslist/browserslist#queries
+                    ['@babel/preset-env', {
+                        targets: {
+                            // > 5% in KR : 한국에서 점유율이 5% 이상인 browser 지원.
+                            browsers: ['> 5% in KR', 'last 2 chrome versions']
+                            ,
+                        }
+                        , debug: true // babel을 deugging mode로 실행.
+                    }]
+                    , '@babel/preset-react'
+                ]
+                , plugins: []
             }
-        ]
+        }]
     }
+
+    , plugins: [
+        new webpack.LoaderOptionsPlugin({ debug: true })
+        ,
+    ]
 
     , output: {
         path: path.join(__dirname, 'core')
