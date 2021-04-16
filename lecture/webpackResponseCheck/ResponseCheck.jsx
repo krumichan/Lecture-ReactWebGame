de0744463830c1a3517fa431bf9e12dda87eb8f7
KrumiChan <lovelybarry@naver.com>
2021-04-16 14:12:37 +0900

    [webpack] response check skeleton 제작.

---
+++ lecture/webpackResponseCheck/ResponseCheck.jsx
@@ -0,0 +1,7 @@
+import React, { PureComponent } from 'react';
+
+class ResponseCheck extends PureComponent {
+
+}
+
+export default ResponseCheck;
---
+++ lecture/webpackResponseCheck/client.jsx
@@ -0,0 +1,10 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+import ResponseCheck from './ResponseCheck'
+
+ReactDOM.render(
+    <>
+        <ResponseCheck/>
+    </>
+    , document.querySelector('#root'));
