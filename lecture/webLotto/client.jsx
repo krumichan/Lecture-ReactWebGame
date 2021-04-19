088cd13b06e3c2e5c15eeaa898330594419c72d1
KrumiChan <lovelybarry@naver.com>
2021-04-19 13:40:09 +0900

    [webpack] lotto skeleton 제작.

---
+++ lecture/webLotto/client.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Lotto from './Lotto';
+
+ReactDOM.render(
+    <>
+        <h1>Lotto Class</h1>
+        <Lotto/>
+        <hr/>
+
+        <h1>Lotto Hooks</h1>
+    </>
+, document.querySelector('#root'));
